4d98136906405c1f05b24cfae818fddf021dd602
nugrahaindie <nugraha@indietechsolutions.com>
2025-01-03 15:23:35 +0700

    add static folder images

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+// inside public directory.
+app.use(express.static('public'));
+app.use('/images', express.static('images'));
+
 const webRoutes = require('./routes/web');
 
 app.use('/', webRoutes);
